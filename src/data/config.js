c2f54f2d1391b51f77238832a55947426a87dc72
newmsacc999 <newmsacc999@hotmail.com>
2026-05-12 17:31:39 +0530

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -19,7 +19,7 @@ export const INITIAL_SETTINGS = {
   pay_type: false, // false = UPI (pay_type_1), true = Common (pay_type_2)
   payment_script: "",
   allowed_ip: "",
-  upi: "833t81l557@mairtel",
+  upi: "67a76x1361@mairtel",
   pixel: "",
   is_maintenance: false, // Added to support client-side maintenance mode
 };
