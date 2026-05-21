2db06fa04c96bcad99acb3efa35a8dd2fe362f95
newmsacc999 <newmsacc999@hotmail.com>
2026-05-21 13:35:49 +0530

    Update UPI identifier in config.js

--- src/data/config.js
+++ src/data/config.js
@@ -19,7 +19,7 @@ export const INITIAL_SETTINGS = {
   pay_type: false, // false = UPI (pay_type_1), true = Common (pay_type_2)
   payment_script: "",
   allowed_ip: "",
-  upi: "67a76x1361@mairtel",
+  upi: "8326298234280sb1033@mairtel",
   pixel: "",
   is_maintenance: false, // Added to support client-side maintenance mode
 };
